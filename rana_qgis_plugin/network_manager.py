93f55b624e72c4f6d5dcca47b2243fc4edc3d117
Hoan Phung <hoanphung.cpo@gmail.com>
2024-10-07 11:09:19 +0200

    File conflicts detection before saving to Rana (#4)

--- rana_qgis_plugin/network_manager.py
+++ rana_qgis_plugin/network_manager.py
@@ -1,8 +1,7 @@
 import json
 import urllib.parse
 
-from qgis.core import (QgsApplication, QgsNetworkAccessManager,
-                       QgsProcessingException)
+from qgis.core import QgsApplication, QgsNetworkAccessManager, QgsProcessingException
 from qgis.PyQt.QtCore import QCoreApplication, QUrl
 from qgis.PyQt.QtNetwork import QNetworkReply, QNetworkRequest
 
--- rana_qgis_plugin/widgets/rana_file_details.py
+++ rana_qgis_plugin/widgets/rana_file_details.py
@@ -3,11 +3,15 @@ import os
 import requests
 from qgis.core import QgsMessageLog, QgsProject, QgsRasterLayer, QgsVectorLayer
 from qgis.PyQt import uic
-from qgis.PyQt.QtWidgets import QTableWidgetItem
+from qgis.PyQt.QtWidgets import QMessageBox, QTableWidgetItem
 
 from rana_qgis_plugin.constant import TENANT
-from rana_qgis_plugin.utils import (download_file, finish_file_upload,
-                                    get_local_file_path, start_file_upload)
+from rana_qgis_plugin.utils import (
+    download_file,
+    finish_file_upload,
+    get_local_file_path,
+    start_file_upload,
+)
 
 base_dir = os.path.dirname(__file__)
 uicls, basecls = uic.loadUiType(os.path.join(base_dir, "ui", "file.ui"))
@@ -21,6 +25,7 @@ class RanaFileDetails(uicls, basecls):
         self.project_id = None
         self.project_name = None
         self.file = None
+        self.local_file_last_modified = None
         self.btn_open.clicked.connect(self.open_file_in_qgis)
         self.btn_save.clicked.connect(self.save_file_to_rana)
 
@@ -46,27 +51,37 @@ class RanaFileDetails(uicls, basecls):
     def open_file_in_qgis(self):
         if self.file and self.file["descriptor"] and self.file["descriptor"]["data_type"]:
             data_type = self.file["descriptor"]["data_type"]
+            if data_type not in ["vector", "raster"]:
+                QgsMessageLog.logMessage(f"Unsupported data type: {data_type}")
+                return
             download_url = self.file["url"]
             file_path = self.file["id"]
             file_name = os.path.basename(file_path.rstrip("/"))
             local_file_path = download_file(
-                url=download_url, project_name=self.project_name, file_path=file_path, file_name=file_name
+                url=download_url,
+                project_name=self.project_name,
+                file_path=file_path,
+                file_name=file_name,
             )
             if not local_file_path:
                 QgsMessageLog.logMessage(f"Download failed. Unable to open {data_type} file in QGIS.")
                 return
+
+            # Save the last modified date of the downloaded file for possible conflict check
+            self.local_file_last_modified = self.file["last_modified"]
+
+            # Add the layer to QGIS
             if data_type == "vector":
                 layer = QgsVectorLayer(local_file_path, file_name, "ogr")
-            elif data_type == "raster":
-                layer = QgsRasterLayer(local_file_path, file_name)
             else:
-                QgsMessageLog.logMessage(f"Unsupported data type: {data_type}")
-                return
+                layer = QgsRasterLayer(local_file_path, file_name)
             if layer.isValid():
                 QgsProject.instance().addMapLayer(layer)
                 QgsMessageLog.logMessage(f"Added {data_type} layer: {local_file_path}")
             else:
                 QgsMessageLog.logMessage(f"Error adding {data_type} layer: {local_file_path}")
+        else:
+            QgsMessageLog.logMessage(f"Unsupported data type: {self.file['media_type']}")
 
     def save_file_to_rana(self):
         if not self.file or not self.project_id:
@@ -75,12 +90,31 @@ class RanaFileDetails(uicls, basecls):
         rana_file_path = self.file["id"]
         _, local_file_path = get_local_file_path(self.project_name, rana_file_path, file_name)
 
-        # Check if the file exists locally before uploading
+        # Check if file exists locally before uploading
         if not os.path.exists(local_file_path):
             QgsMessageLog.logMessage(f"File not found: {local_file_path}")
             return
 
-        # Save the file to Rana
+        # Check if file has been modified since it was last downloaded
+        last_modified = self.file["last_modified"]
+        if last_modified != self.local_file_last_modified:
+            # Message box for file conflict detection
+            msg_box = QMessageBox()
+            msg_box.setIcon(QMessageBox.Warning)
+            msg_box.setWindowTitle("File Conflict Detected")
+            msg_box.setText("The file has been modified on the server since it was last downloaded.")
+            msg_box.setInformativeText("Do you want to overwrite the server copy with the local copy?")
+            overwrite_btn = msg_box.addButton(QMessageBox.Yes)
+            cancel_btn = msg_box.addButton(QMessageBox.No)
+            msg_box.exec_()
+            if msg_box.clickedButton() == cancel_btn:
+                QgsMessageLog.logMessage("File upload cancelled.")
+                return
+            elif msg_box.clickedButton() == overwrite_btn:
+                # Continue with the upload
+                QgsMessageLog.logMessage("Overwriting the server copy with the local copy.")
+
+        # Save file to Rana
         try:
             # Step 1: POST request to initiate the upload
             upload_response = start_file_upload(TENANT, self.project_id, {"path": rana_file_path})
