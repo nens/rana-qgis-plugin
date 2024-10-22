ffb6436d6f64d8bffb1c55232f6b7aff6ea17808
Hoan Phung <hoanphung.cpo@gmail.com>
2024-10-22 15:21:50 +0200

    Fix name of the plugin in zip_plugin

--- zip_plugin.py
+++ zip_plugin.py
@@ -1,4 +1,4 @@
-# 3Di Models and Simulations for QGIS, licensed under GPLv2 or (at your option) any later version
+# Rana QGIS Plugin for QGIS, licensed under GPLv2 or (at your option) any later version
 # Copyright (C) 2023 by Lutra Consulting for 3Di Water Management
 import os
 import re
