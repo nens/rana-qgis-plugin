b24b57ca2894908351333e51b2505de5ba01f4ed
Hoan Phung <hoanphung.cpo@gmail.com>
2025-03-06 10:17:32 +0100

    generate vector styles

--- rana_qgis_plugin/constant.py
+++ rana_qgis_plugin/constant.py
@@ -6,13 +6,13 @@ RANA_AUTHCFG_ENTRY = f"{RANA_SETTINGS_ENTRY}/authcfg"
 RANA_TENANT_ENTRY = f"{RANA_SETTINGS_ENTRY}/tenant"
 THREEDI_AUTHCFG_ENTRY = "threedi/authcfg"
 
-COGNITO_CLIENT_ID = "250mkcukj5tn6lblsd6ka42c0a"
+COGNITO_CLIENT_ID = "77chge3p2dq74a5uspvt136piu"
 COGNITO_AUTHENTICATION_ENDPOINT = "https://auth.lizard.net/oauth2/authorize"
 COGNITO_TOKEN_ENDPOINT = "https://auth.lizard.net/oauth2/token"
 COGNITO_USER_INFO_ENDPOINT = "https://auth.lizard.net/oauth2/userInfo"
 COGNITO_LOGOUT_ENDPOINT = "https://auth.lizard.net/logout"
 
-BASE_URL = "https://www.ranawaterintelligence.com"
+BASE_URL = "https://test.ranawaterintelligence.com"
 API_URL = f"{BASE_URL}/v1-alpha"
 LOGOUT_REDIRECT_URI = f"{BASE_URL}/auth/callback/cognito/logout"
 LOGOUT_REDIRECT_URI_ENCODED = quote(LOGOUT_REDIRECT_URI, safe="")
