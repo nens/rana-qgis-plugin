6c6ec91bbf0e0f71763aef596fcd0a7ac81d88bb
elisalle <eli.salle@nelen-schuurmans.nl>
2025-06-05 17:21:41 +0200

    Revert "Refactor vector style worker method (#28)"
    This reverts commit 6b166b3c2068b6d92bcd9648d53b6f6fe1f83f05.

--- rana_qgis_plugin/workers.py
+++ rana_qgis_plugin/workers.py
@@ -96,29 +96,6 @@ class FileUploadWorker(QThread):
         self.last_modified = None
         self.last_modified_key = f"{project['name']}/{file['id']}/last_modified"
 
-    def upload_to_s3(self, url: str, data: dict, content_type: str):
-        """Method to upload to S3"""
-        try:
-            headers = {"Content-Type": content_type}
-            response = requests.put(url, data=data, headers=headers)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            self.failed.emit(f"Failed to upload file to S3: {str(e)}")
-
-    def create_qml_zip(self, local_dir: str, zip_path: str):
-        """Craete a QML zip file for all the qml files in the local directory"""
-        try:
-            with zipfile.ZipFile(zip_path, "w") as zip_file:
-                for root, _, files in os.walk(local_dir):
-                    for file in files:
-                        if file.endswith(".qml"):
-                            file_path = os.path.join(root, file)
-                            zip_file.write(
-                                file_path, os.path.relpath(file_path, local_dir)
-                            )
-        except Exception as e:
-            self.failed.emit(f"Failed to create QML zip: {str(e)}")
-
     def handle_file_conflict(self):
         file_path = self.file["id"]
         local_last_modified = QSettings().value(self.last_modified_key)
@@ -141,13 +118,9 @@ class FileUploadWorker(QThread):
     @pyqtSlot()
     def run(self):
         if not self.file or not self.project["id"]:
-            self.failed.emit("File / project cannot be found.")
             return
         project_slug = self.project["slug"]
-        descriptor_id = self.file["descriptor_id"]
-        data_type = self.file["data_type"]
         path = self.file["id"]
-        file_name = os.path.basename(path.rstrip("/"))
         _, local_file_path = get_local_file_path(project_slug, path)
 
         # Check if file exists locally before uploading
@@ -160,90 +133,7 @@ class FileUploadWorker(QThread):
         if not continue_upload:
             return
 
-        # Upload vector styles to Rana if applicable
-        if data_type == "vector":
-            # Check if layers are available in QGIS
-            all_layers = QgsProject.instance().mapLayers().values()
-            layers = [layer for layer in all_layers if file_name in layer.source()]
-
-            if not layers:
-                self.failed.emit(
-                    f"No layers found for {file_name}. Open the file in QGIS and try again."
-                )
-                return
-
-            # Save vector styling files to Rana
-            qgis_layers = {layer.name(): layer for layer in layers}
-            group = {"layers": list(qgis_layers.keys())}
-            base_url = "http://baseUrl"
-
-            # Save QML style files for each layer to local directory
-            local_dir, _ = get_local_file_path(self.project["slug"], path)
-            os.makedirs(local_dir, exist_ok=True)
-            for layer in layers:
-                qml_path = os.path.join(local_dir, f"{layer.name()}.qml")
-                layer.saveNamedStyle(str(qml_path))
-
-            # Convert QGIS layers to styling files for the Rana Web Client
-            try:
-                self.progress.emit(0)
-                _, warnings, mb_style, sprite_sheet = convertGroup(
-                    group, qgis_layers, base_url, workspace="workspace", name="default"
-                )
-                if warnings:
-                    self.warning.emit(", ".join(warnings))
-
-                # Get upload URLs to S3
-                upload_urls = get_vector_style_upload_urls(descriptor_id)
-
-                if not upload_urls:
-                    self.failed.emit(
-                        "Failed to get vector style upload URLs from the API."
-                    )
-                    return
-
-                # Upload style.json
-                self.upload_to_s3(
-                    upload_urls["style.json"], json.dumps(mb_style), "application/json"
-                )
-
-                # Upload sprite images if available
-                if (
-                    sprite_sheet
-                    and sprite_sheet.get("img")
-                    and sprite_sheet.get("img2x")
-                ):
-                    self.upload_to_s3(
-                        upload_urls["sprite.png"],
-                        image_to_bytes(sprite_sheet["img"]),
-                        "image/png",
-                    )
-                    self.upload_to_s3(
-                        upload_urls["sprite@2x.png"],
-                        image_to_bytes(sprite_sheet["img2x"]),
-                        "image/png",
-                    )
-                    self.upload_to_s3(
-                        upload_urls["sprite.json"],
-                        sprite_sheet["json"],
-                        "application/json",
-                    )
-                    self.upload_to_s3(
-                        upload_urls["sprite@2x.json"],
-                        sprite_sheet["json2x"],
-                        "application/json",
-                    )
-
-                # Zip and upload QML zip
-                zip_path = os.path.join(local_dir, "qml.zip")
-                self.create_qml_zip(local_dir, zip_path)
-                with open(zip_path, "rb") as file:
-                    self.upload_to_s3(upload_urls["qml.zip"], file, "application/zip")
-                os.remove(zip_path)
-            except Exception as e:
-                self.warning.emit(f"Failed to convert styling files: {str(e)}")
-
-        # Upload file to Rana
+        # Save file to Rana
         try:
             self.progress.emit(0)
             # Step 1: POST request to initiate the upload
@@ -271,3 +161,111 @@ class FileUploadWorker(QThread):
             self.finished.emit()
         except Exception as e:
             self.failed.emit(f"Failed to upload file to Rana: {str(e)}")
+
+
+class VectorStyleWorker(QThread):
+    """Worker thread for generating vector styling files"""
+
+    finished = pyqtSignal(str)
+    failed = pyqtSignal(str)
+    warning = pyqtSignal(str)
+
+    def __init__(
+        self,
+        project: dict,
+        file: dict,
+    ):
+        super().__init__()
+        self.project = project
+        self.file = file
+
+    def upload_to_s3(self, url: str, data: dict, content_type: str):
+        """Method to upload to S3"""
+        try:
+            headers = {"Content-Type": content_type}
+            response = requests.put(url, data=data, headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            self.failed.emit(f"Failed to upload file to S3: {str(e)}")
+
+    def create_qml_zip(self, local_dir: str, zip_path: str):
+        """Craete a QML zip file for all the qml files in the local directory"""
+        try:
+            with zipfile.ZipFile(zip_path, "w") as zip_file:
+                for root, _, files in os.walk(local_dir):
+                    for file in files:
+                        if file.endswith(".qml"):
+                            file_path = os.path.join(root, file)
+                            zip_file.write(file_path, os.path.relpath(file_path, local_dir))
+        except Exception as e:
+            self.failed.emit(f"Failed to create QML zip: {str(e)}")
+
+    @pyqtSlot()
+    def run(self):
+        if not self.file:
+            self.failed.emit("File not found.")
+            return
+        path = self.file["id"]
+        file_name = os.path.basename(path.rstrip("/"))
+        descriptor_id = self.file["descriptor_id"]
+        all_layers = QgsProject.instance().mapLayers().values()
+        layers = [layer for layer in all_layers if file_name in layer.source()]
+
+        if not layers:
+            self.failed.emit(
+                f"No layers found for {file_name}. Open the file in QGIS and try again."
+            )
+            return
+
+        qgis_layers = {layer.name(): layer for layer in layers}
+        group = {"layers": list(qgis_layers.keys())}
+        base_url = "http://baseUrl"
+
+        # Save QML style files for each layer to local directory
+        local_dir, _ = get_local_file_path(self.project["slug"], path)
+        os.makedirs(local_dir, exist_ok=True)
+        for layer in layers:
+            qml_path = os.path.join(local_dir, f"{layer.name()}.qml")
+            layer.saveNamedStyle(str(qml_path))
+
+        # Convert QGIS layers to styling files for the Rana Web Client
+        try:
+            self.progress.emit(0)
+            _, warnings, mb_style, sprite_sheet = convertGroup(
+                group, qgis_layers, base_url, workspace="workspace", name="default"
+            )
+            if warnings:
+                self.warning.emit(", ".join(warnings))
+
+            # Get upload URLs to S3
+            upload_urls = get_vector_style_upload_urls(descriptor_id)
+
+            if not upload_urls:
+                self.failed.emit(
+                    "Failed to get vector style upload URLs from the API."
+                )
+                return
+
+            # Upload style.json
+            self.upload_to_s3(
+                upload_urls["style.json"], json.dumps(mb_style), "application/json"
+            )
+
+            # Upload sprite images if available
+            if sprite_sheet and sprite_sheet.get("img") and sprite_sheet.get("img2x"):
+                self.upload_to_s3(upload_urls["sprite.png"], image_to_bytes(sprite_sheet["img"]), "image/png")
+                self.upload_to_s3(upload_urls["sprite@2x.png"], image_to_bytes(sprite_sheet["img2x"]), "image/png")
+                self.upload_to_s3(upload_urls["sprite.json"], sprite_sheet["json"], "application/json")
+                self.upload_to_s3(upload_urls["sprite@2x.json"], sprite_sheet["json2x"], "application/json")
+
+            # Zip and upload QML zip
+            zip_path = os.path.join(local_dir, "qml.zip")
+            self.create_qml_zip(local_dir, zip_path)
+            with open(zip_path, "rb") as file:
+                self.upload_to_s3(upload_urls["qml.zip"], file, "application/zip")
+            os.remove(zip_path)
+
+            # Finish
+            self.finished.emit(f"Styling files uploaded successfully for {file_name}.")
+        except Exception as e:
+            self.failed.emit(f"Failed to generate and upload styling files: {str(e)}")
