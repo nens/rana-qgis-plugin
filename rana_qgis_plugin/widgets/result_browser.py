d2d870258dce8268d047223f43a29f90643d8a06
elisalle <eli.salle@nelen-schuurmans.nl>
2025-11-26 14:09:07 +0100

    split rana and postprocessed rasters into separate table

--- rana_qgis_plugin/widgets/result_browser.py
+++ rana_qgis_plugin/widgets/result_browser.py
@@ -28,15 +28,25 @@ class ResultBrowser(QDialog):
         results_group = QGroupBox("Results", self)
         results_group.setLayout(QGridLayout())
 
-        self.table = QTableWidget(self)
-        results_group.layout().addWidget(self.table)
-        self.table.setColumnCount(2)
-        self.table.verticalHeader().setVisible(False)
-        self.table.horizontalHeader().setStretchLastSection(True)
-        self.table.setHorizontalHeaderLabels(["Type", "File name"])
+        postprocessed_rasters_group = QGroupBox("Postprocess raster results (slow)", self)
+        postprocessed_rasters_group.setLayout(QGridLayout())
 
-        for i, result in enumerate([r for r in results if r["attachment_url"] or r["raster_id"]]):
-            self.table.insertRow(self.table.rowCount())
+        self.results_table = QTableWidget(self)
+        results_group.layout().addWidget(self.results_table)
+        self.results_table.setColumnCount(2)
+        self.results_table.verticalHeader().setVisible(False)
+        self.results_table.horizontalHeader().setStretchLastSection(True)
+        self.results_table.setHorizontalHeaderLabels(["Type", "File name"])
+
+        self.postprocessed_rasters_table = QTableWidget(self)
+        postprocessed_rasters_group.layout().addWidget(self.postprocessed_rasters_table)
+        self.postprocessed_rasters_table.setColumnCount(2)
+        self.postprocessed_rasters_table.verticalHeader().setVisible(False)
+        self.postprocessed_rasters_table.horizontalHeader().setStretchLastSection(True)
+        self.postprocessed_rasters_table.setHorizontalHeaderLabels(["Type", "File name"])
+
+        for i, result in enumerate([r for r in results if r["attachment_url"]]):
+            self.results_table.insertRow(self.results_table.rowCount())
             type_item = QTableWidgetItem(result["name"])
             type_item.setFlags(
                 Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsUserCheckable
@@ -44,17 +54,36 @@ class ResultBrowser(QDialog):
             type_item.setCheckState(Qt.CheckState.Unchecked)
             type_item.setData(Qt.ItemDataRole.UserRole, int(result["id"]))
 
-            if result["attachment_url"]:
-                file_name = get_filename_from_attachment_url(result["attachment_url"])
-            else:
-                file_name = result["code"]
+            file_name = get_filename_from_attachment_url(result["attachment_url"])
+
             file_name_item = QTableWidgetItem(file_name)
             file_name_item.setFlags(Qt.ItemFlag.ItemIsEnabled)
-            self.table.setItem(i, 0, type_item)
-            self.table.setItem(i, 1, file_name_item)
+            self.results_table.setItem(i, 0, type_item)
+            self.results_table.setItem(i, 1, file_name_item)
 
-        self.table.resizeColumnsToContents()
+        # timeseries rasters
+        excluded_rasters = ["depth-dtri", "rain-quad", "s1-dtri"]
+
+        for i, result in enumerate([r for r in results if r["raster_id"] and r["code"] not in excluded_rasters]):
+            self.postprocessed_rasters_table.insertRow(self.postprocessed_rasters_table.rowCount())
+            type_item = QTableWidgetItem(result["name"])
+            type_item.setFlags(
+                Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsUserCheckable
+            )
+            type_item.setCheckState(Qt.CheckState.Unchecked)
+            type_item.setData(Qt.ItemDataRole.UserRole, int(result["id"]))
+
+            file_name = result["code"]
+            file_name_item = QTableWidgetItem(file_name)
+            file_name_item.setFlags(Qt.ItemFlag.ItemIsEnabled)
+            self.postprocessed_rasters_table.setItem(i, 0, type_item)
+            self.postprocessed_rasters_table.setItem(i, 1, file_name_item)
+
+        self.results_table.resizeColumnsToContents()
         layout.addWidget(results_group)
+
+        self.postprocessed_rasters_table.resizeColumnsToContents()
+        layout.addWidget(postprocessed_rasters_group)
 
         buttonBox = QDialogButtonBox(
             QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
@@ -68,8 +97,14 @@ class ResultBrowser(QDialog):
 
     def accept(self) -> None:
         self.selected_results = []
-        for r in range(self.table.rowCount()):
-            name_item = self.table.item(r, 0)
+        for r in range(self.results_table.rowCount()):
+            name_item = self.results_table.item(r, 0)
+            if name_item.checkState() == Qt.CheckState.Checked:
+                id = int(name_item.data(Qt.ItemDataRole.UserRole))
+                self.selected_results.append(id)
+
+        for r in range(self.postprocessed_rasters_table.rowCount()):
+            name_item = self.postprocessed_rasters_table.item(r, 0)
             if name_item.checkState() == Qt.CheckState.Checked:
                 id = int(name_item.data(Qt.ItemDataRole.UserRole))
                 self.selected_results.append(id)
