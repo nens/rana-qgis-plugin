839a869c4a4e63ef4b84e559cbc8692155f2f52d
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2026-01-16 14:00:17 +0100

    Use models for tasks and generalize adding tasks

--- rana_qgis_plugin/widgets/tasks_browser.py
+++ rana_qgis_plugin/widgets/tasks_browser.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from qgis.PyQt.QtCore import (
     Qt,
     QTimer,
@@ -18,6 +20,56 @@ from rana_qgis_plugin.utils import get_timestamp_as_numeric_item
 from rana_qgis_plugin.utils_api import get_user_by_email
 
 
+@dataclass
+class TaskData:
+    id: int
+    name: str
+    user_email: str
+    created: str
+    status: str
+    progress: int
+    max_progress: int
+
+    def progress_str(self):
+        raise NotImplementedError
+
+
+@dataclass
+class SimulationTaskData(TaskData):
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id=data["uid"],
+            name=data["name"],
+            user_email=data["user_name"],
+            created=data["date_created"],
+            status=data["status"],
+            progress=int(data["progress"]),
+            max_progress=100,
+        )
+
+    def progress_str(self):
+        return f"{self.status} ({self.progress}%)"
+
+
+@dataclass
+class ModelTaskData(TaskData):
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id=data["id"],
+            name=data["name"],
+            user_email=data["user_email"],
+            created=data["created"],
+            status=data["status"],
+            progress=int(data["finished_tasks"]),
+            max_progress=int(data["total_tasks"]),
+        )
+
+    def progress_str(self):
+        return f"{self.status} ({self.progress} of {self.max_progress})"
+
+
 class TasksBrowser(QWidget):
     start_monitoring_simulations = pyqtSignal()
     start_monitoring_model_generation = pyqtSignal()
@@ -32,12 +84,10 @@ class TasksBrowser(QWidget):
         self.communication = communication
         self.tasks = []
         self.setup_ui()
-        # TODO: use a more elegant solution, like a model
         self.simulation_row_map = {}
         self.model_row_map = {}
         QTimer.singleShot(0, lambda: self.start_monitoring_simulations.emit())
         QTimer.singleShot(0, lambda: self.start_monitoring_model_generation.emit())
-        # self.populate_tasks()
 
     def setup_ui(self):
         # TODO: consider using a custom model
@@ -55,70 +105,58 @@ class TasksBrowser(QWidget):
         self.tasks_model.appendRow([self.simulation_root])
         self.tasks_model.appendRow([self.models_root])
 
-    def update_simulation_task(self, task_data):
-        row = self.simulation_row_map.get(task_data["uid"])
-        if not row:
-            return
-        status_item = self.simulation_root.child(row, 3)
-        status_item.setData(task_data["status"], Qt.ItemDataRole.UserRole)
-        progress_bar = self.tasks_tv.indexWidget(status_item.index())
-        progress = int(task_data["progress"])
-        progress_bar.setValue(progress)
-        progress_bar.setFormat(f"{task_data['status']} ({progress}%)")
+    def add_task(self, task, root, row_map):
+        name_item = QStandardItem(task.name)
+        # TODO: link to rana user and show icon - wait for open PRs
+        user = get_user_by_email(task.user_email)
+        user_str = f"{user['given_name'][0]}{user['family_name'][0]}" if user else "?"
+        who_item = QStandardItem(user_str)
+        date_item = get_timestamp_as_numeric_item(task.created)
+        status_item = QStandardItem()
+        status_item.setData(task.status, Qt.ItemDataRole.UserRole)
+        # Create the progress bar
+        progress_bar = QProgressBar()
+        self.update_pb_progress(progress_bar, task)
+        progress_bar.setTextVisible(True)
+        row = [name_item, who_item, date_item, status_item]
+        root.appendRow(row)
+        self.tasks_tv.setIndexWidget(status_item.index(), progress_bar)
+        row_map[task.id] = root.rowCount() - 1
+
+    @staticmethod
+    def update_pb_progress(progress_bar, task):
+        progress_bar.setValue(task.progress)
+        progress_bar.setMaximum(task.max_progress)
+        progress_bar.setFormat(task.progress_str())
 
     def add_simulation_task(self, task_data):
-        root = self.simulation_root
-        name_item = QStandardItem(task_data["name"])
-        # TODO: link to rana user and show icon - wait for open PRs
-        user = get_user_by_email(task_data["user_name"])
-        user_str = f"{user['given_name'][0]}{user['family_name'][0]}" if user else "?"
-        who_item = QStandardItem(user_str)
-        date_item = get_timestamp_as_numeric_item(task_data["date_created"])
-        status_item = QStandardItem()
-        status_item.setData(task_data["status"], Qt.ItemDataRole.UserRole)
-        # Create the progress bar
-        progress_bar = QProgressBar()
-        progress_bar.setValue(int(task_data["progress"]))
-        progress_bar.setMaximum(100)
-        progress_bar.setFormat(f"{task_data['status']} ({int(task_data['progress'])}%)")
-        progress_bar.setTextVisible(True)
-        row = [name_item, who_item, date_item, status_item]
-        root.appendRow(row)
-        self.tasks_tv.setIndexWidget(status_item.index(), progress_bar)
-        self.simulation_row_map[task_data["uid"]] = root.rowCount() - 1
+        self.add_task(
+            SimulationTaskData.from_dict(task_data),
+            self.simulation_root,
+            self.simulation_row_map,
+        )
 
     def add_model_task(self, task_data):
-        root = self.models_root
-        name_item = QStandardItem(task_data["name"])
-        # TODO: link to rana user and show icon - wait for open PRs
-        user = get_user_by_email(task_data["user_email"])
-        user_str = f"{user['given_name'][0]}{user['family_name'][0]}" if user else "?"
-        who_item = QStandardItem(user_str)
-        date_item = get_timestamp_as_numeric_item(task_data["created"])
-        status_item = QStandardItem()
-        status_item.setData(task_data["status"], Qt.ItemDataRole.UserRole)
-        # Create the progress bar
-        progress_bar = QProgressBar()
-        progress_bar.setValue(int(task_data["finished_tasks"]))
-        progress_bar.setMaximum(int(task_data["total_tasks"]))
-        progress_bar.setFormat(
-            f"{task_data['status']} ({progress_bar.value()} of {progress_bar.maximum()})"
+        self.add_task(
+            ModelTaskData.from_dict(task_data), self.models_root, self.model_row_map
         )
-        progress_bar.setTextVisible(True)
-        row = [name_item, who_item, date_item, status_item]
-        root.appendRow(row)
-        self.tasks_tv.setIndexWidget(status_item.index(), progress_bar)
-        self.model_row_map[task_data["id"]] = root.rowCount() - 1
 
-    def update_model_task(self, task_data):
-        row = self.model_row_map.get(task_data["id"], -1)
+    def update_task(self, task, root, row_map):
+        row = row_map.get(task.id, -1)
         if row < 0:
             return
-        status_item = self.models_root.child(row, 3)
-        status_item.setData(task_data["status"], Qt.ItemDataRole.UserRole)
-        progress_bar = self.tasks_tv.indexWidget(status_item.index())
-        progress_bar.setValue(int(task_data["finished_tasks"]))
-        progress_bar.setMaximum(int(task_data["total_tasks"]))
-        progress_bar.setFormat(
-            f"{task_data['status']} ({progress_bar.value()} of {progress_bar.maximum()})"
+        status_item = root.child(row, 3)
+        status_item.setData(task.status, Qt.ItemDataRole.UserRole)
+        self.update_pb_progress(self.tasks_tv.indexWidget(status_item.index()), task)
+
+    def update_simulation_task(self, task_data):
+        self.update_task(
+            SimulationTaskData.from_dict(task_data),
+            self.simulation_root,
+            self.simulation_row_map,
+        )
+
+    def update_model_task(self, task_data):
+        self.update_task(
+            ModelTaskData.from_dict(task_data), self.models_root, self.model_row_map
         )
