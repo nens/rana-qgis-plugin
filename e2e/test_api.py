688c51023a404c9a033e938deed71db0b9672085
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2026-03-02 16:24:49 +0100

    wip1

--- e2e/test_api.py
+++ e2e/test_api.py
@@ -7,14 +7,14 @@ from qgis.PyQt.QtTest import QTest
 def test_smoke(plugin):
     rana_tool_button = plugin.toolbar.widgetForAction(plugin.action)
     # QTest.mouseClick(rana_tool_button, Qt.LeftButton)
-    sleep(10)
-    rana_tool_button.click()
+    # sleep(10)
+    # rana_tool_button.click()
 
-    print("*******")
-    print(plugin.rana_browser.projects_browser.projects_tv.model().rowCount())
-    # import pdb
-    # pdb.set_trace()
-    # plugin.run()
-    # print(dir(plugin))
-    sleep(10)
+    # print("*******")
+    # print(plugin.rana_browser.projects_browser.projects_tv.model().rowCount())
+    # # import pdb
+    # # pdb.set_trace()
+    # # plugin.run()
+    # # print(dir(plugin))
+    # sleep(10)
     assert True
