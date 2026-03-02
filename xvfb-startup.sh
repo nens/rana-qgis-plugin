f9c8c72fedb8fa8ab49e743f3cbf133bb89c647a
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2026-03-02 11:20:29 +0100

    wip

--- xvfb-startup.sh
+++ xvfb-startup.sh
@@ -4,4 +4,10 @@ XVFB_PROC=$!
 sleep 1
 export DISPLAY=:99
 "$@"
+
+EXIT_CODE=$?   # Capture the exit code of pytest
+
 kill $XVFB_PROC
+
+# Exit the container with pytest's exit code
+exit $EXIT_CODE
