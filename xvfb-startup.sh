f88cda0db5f84e427c226b575f9fe81c074e6bca
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2026-03-10 12:27:54 +0100

    clean up

--- xvfb-startup.sh
+++ xvfb-startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Set up virtual screen
 Xvfb :99 -ac -screen 0 "$XVFB_RES" -nolisten tcp $XVFB_ARGS &
 XVFB_PROC=$!
 export DISPLAY=:99
@@ -7,7 +8,7 @@ sleep 1
 # Start window manager
 fluxbox &
 
-# Start VNC server
+# Start VNC server for visual inspection (optional)
 x11vnc -display :99 -nopw -forever -shared &
 
 ffmpeg -f x11grab -video_size 1920x1080 -framerate 15 -i :99 -c:v libx264 -preset veryfast -crf 28 -pix_fmt yuv420p -movflags +faststart output.mp4 &
@@ -18,10 +19,9 @@ sleep 1
 "$@"
 EXIT_CODE=$?   # Capture the exit code of pytest
 
-# Stop ffmpeg cleanly
+# Stop ffmpeg and xvfb cleanly
 kill -INT $FFMPEG_PROC
 wait $FFMPEG_PROC
-
 kill $XVFB_PROC
 
 # Exit the container with pytest's exit code
